bc56fe75a3d10c6980aa96d802cfc3dd1e8121e5
kabelo-radi <111067256+kabelo-radi@users.noreply.github.com>
2025-02-13 16:38:59 +0200

    Add files via upload

---
+++ MiniBank/src/minibank/MiniBank.java
@@ -0,0 +1,16 @@
+
+package minibank;
+
+
+public class MiniBank {
+
+    public static void main(String[] args) {
+       new SignInAd().setVisible(true);
+       // new AdminGui().setVisible(true);
+       //new CheckDB().setVisible(true);
+       //new ChangeAcc_1().setVisible(true);
+       //new CreditsLoans().setVisible(true);
+       //new ATM().setVisible(true);
+    }
+    
+}
